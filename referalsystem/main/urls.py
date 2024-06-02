3473bcf2e3e1a3a9d4f44841cb7290b8a5d6d392
SweetKitt <Faite@bk.ru>
2024-06-02 13:40:43 +0300

    checking invite-code

--- referalsystem/main/urls.py
+++ referalsystem/main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.get_sms_code, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('code/', views.confirm_sms_code, name='code'),
-    path('invite/', views.invite, name='invite'),
+    path('login/code/', views.confirm_sms_code, name='code'),
+    path('home/invite/', views.check_invite, name='invite'),
     path('home/', views.home, name='home'),
     ]
--- referalsystem/main/views.py
+++ referalsystem/main/views.py
@@ -9,6 +9,7 @@ from django.utils import timezone
 from .forms import UserForm
 from .models import User, SmsCode
 
+
 SMS_CODE_LIFE_IN_SECONDS = 180
 
 
@@ -29,33 +30,45 @@ def confirm_sms_code(request):
     phone_number = request.POST['phone_number']
     sms_code = request.POST['sms_code']
     verify = SmsCode.objects.filter(phone_number=phone_number).latest('date')
-    if verify.code == sms_code:
-        code_time = timezone.now() - verify.date
-        if verify.used == False and code_time.seconds <= SMS_CODE_LIFE_IN_SECONDS:
-            verify.used = True
-            try:
-                user = User.objects.get(phone_number=phone_number)
-            except ObjectDoesNotExist:
-                invite_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-                user = User.objects.create(
-                                    phone_number=phone_number,
-                                    invite_code=invite_code)
-            login(request, user)
-            return render(request, 'main/home.html', {'user': user})
-        else:
-            error = 'Смс-код устарел'
-            return render(request, 'registration/code.html', {'sms_code': verify, 'error': error })
+    if verify is not None:
+        if verify.code == sms_code:
+            code_time = timezone.now() - verify.date
+            if verify.used == False and code_time.seconds <= SMS_CODE_LIFE_IN_SECONDS:
+                verify.used = True
+                verify.save()
+                try:
+                    user = User.objects.get(phone_number=phone_number)
+                except ObjectDoesNotExist:
+                    invite_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+                    user = User.objects.create(
+                                        phone_number=phone_number,
+                                        invite_code=invite_code)
+                login(request, user)
+                invites = User.objects.filter(invites=user)
+                return render(request, 'main/home.html', {'user': user, 'invites': invites})
+            else:
+                error = 'Смс-код устарел'
+                return render(request, 'registration/code.html', {'sms_code': verify, 'error': error })
     error = 'Не верный смс-код'
     return render(request,'registration/code.html', {'sms_code': verify, 'error': error})
 
 
-def invite(request):
+def check_invite(request):
     invite = request.POST['invite']
+    user_id = request.POST['user_id']
+    user = User.objects.get(id=user_id)
+    if user.invite_code == invite:
+        error = 'Введите чужой инвайт-код'
+        return render(request, 'main/home.html', {'error': error})
     try:
         user_invite = User.objects.get(invite_code=invite)
     except ObjectDoesNotExist:
-        user_invite = None
-    return render(request, 'main/home.html')
+        error = 'Пользователя с таким инвайт-кодом не найден'
+        return render(request, 'main/home.html', {'error': error})
+    user.invites = user_invite
+    user.save()
+    invites = User.objects.filter(invites=user)
+    return render(request, 'main/home.html', {'user': user, 'invites': invites})
 
 
 def logout_view(request):
